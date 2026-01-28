3b8b678543fe5abf3aab4a49f51ebfe456ff7aa9
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-01-28 06:13:01 +0500

    Refactor user management views to use Dashmin template UI elements

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -2,9 +2,9 @@
     <div class="row">
         <div class="col-12 col-md-8 mx-auto">
             <div class="card mb-30">
-                <div class="card-body">
+                <div class="card-body p-30">
                     <div class="d-flex justify-content-between align-items-center mb-4">
-                        <h4 class="card-title">Edit User</h4>
+                        <h4 class="font-20">Edit User</h4>
                         <a href="{{ route('admin.users.index') }}" class="btn btn-secondary btn-sm">
                             <i class="icofont-arrow-left"></i> Back to List
                         </a>
@@ -14,40 +14,60 @@
                         @csrf
                         @method('PUT')
 
-                        <div class="form-group mb-4">
-                            <label for="name">Name</label>
-                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name', $user->name) }}" required>
-                            @error('name')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-                        <div class="form-group mb-4">
-                            <label for="email">Email Address</label>
-                            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email', $user->email) }}" required>
-                            @error('email')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-                        <div class="form-group mb-4">
-                            <label class="d-block mb-2">Roles</label>
+                        <div class="edit-personal-info mb-5">
                             <div class="row">
-                                @foreach($roles as $role)
-                                <div class="col-md-4">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="role_{{ $role->id }}" name="roles[]" value="{{ $role->name }}"
-                                            {{ $user->hasRole($role->name) ? 'checked' : '' }}>
-                                        <label class="custom-control-label" for="role_{{ $role->id }}">
-                                            {{ ucfirst(str_replace('_', ' ', $role->name)) }}
-                                        </label>
-                                    </div>
+                                <div class="col-12">
+                                    <h4 class="mb-3">User Information</h4>
                                 </div>
-                                @endforeach
                             </div>
-                            @error('roles')
-                                <div class="text-danger mt-2 small">{{ $message }}</div>
-                            @enderror
+
+                            <div class="form-group row align-items-center">
+                                <div class="col-3">
+                                    <label for="name">Name</label>
+                                </div>
+                                <div class="col-9">
+                                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name', $user->name) }}" required>
+                                    @error('name')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row align-items-center">
+                                <div class="col-3">
+                                    <label for="email">Email Address</label>
+                                </div>
+                                <div class="col-9">
+                                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email', $user->email) }}" required>
+                                    @error('email')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row align-items-center">
+                                <div class="col-3">
+                                    <label>Roles</label>
+                                </div>
+                                <div class="col-9">
+                                    <div class="row">
+                                        @foreach($roles as $role)
+                                        <div class="col-md-4">
+                                            <div class="custom-control custom-checkbox">
+                                                <input type="checkbox" class="custom-control-input" id="role_{{ $role->id }}" name="roles[]" value="{{ $role->name }}"
+                                                    {{ $user->hasRole($role->name) ? 'checked' : '' }}>
+                                                <label class="custom-control-label" for="role_{{ $role->id }}">
+                                                    {{ ucfirst(str_replace('_', ' ', $role->name)) }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                        @endforeach
+                                    </div>
+                                    @error('roles')
+                                        <div class="text-danger mt-2 small">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                            </div>
                         </div>
 
                         <div class="form-group">
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,11 +2,11 @@
     <div class="row">
         <div class="col-12">
             <div class="card mb-30">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h4 class="card-title">User Management</h4>
-                    </div>
-                    
+                <div class="card-body pt-30">
+                    <h4 class="font-20 ">User Management</h4>
+                </div>
+
+                <div class="px-30">
                     @if(session('success'))
                         <div class="alert alert-success alert-dismissible fade show" role="alert">
                             {{ session('success') }}
@@ -24,63 +24,65 @@
                             </button>
                         </div>
                     @endif
+                </div>
 
-                    <div class="table-responsive">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Roles</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($users as $user)
-                                <tr>
-                                    <td>
-                                        <div class="d-flex align-items-center">
-                                            <div class="img mr-3">
-                                                <img src="{{ $user->profile_photo_url }}" class="rounded-circle" width="40" alt="{{ $user->name }}">
-                                            </div>
-                                            <div class="text">
-                                                <h6 class="mb-0">{{ $user->name }}</h6>
+                <div class="table-responsive">
+                    <table class="text-nowrap hoverable dh-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Roles</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>
+                                    <div class="d-flex align-items-center">
+                                        <div class="img mr-3">
+                                            <img src="{{ $user->profile_photo_url }}" class="rounded-circle" width="40" alt="{{ $user->name }}">
+                                        </div>
+                                        <div class="text">
+                                            <h6 class="mb-0">{{ $user->name }}</h6>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    @foreach($user->roles as $role)
+                                        <div class="chip mr-2" title="{{ ucfirst(str_replace('_', ' ', $role->name)) }}">
+                                            <div class="chip-content {{ $role->name === 'super_admin' ? 'primary' : 'secondary' }}">
+                                                <div class="font-12">{{ ucfirst(str_replace('_', ' ', $role->name)) }}</div>
                                             </div>
                                         </div>
-                                    </td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>
-                                        @foreach($user->roles as $role)
-                                            <span class="badge badge-{{ $role->name === 'super_admin' ? 'primary' : 'secondary' }}">
-                                                {{ ucfirst(str_replace('_', ' ', $role->name)) }}
-                                            </span>
-                                        @endforeach
-                                    </td>
-                                    <td>
-                                        <div class="d-flex">
-                                            <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-info btn-sm mr-2" title="Edit">
-                                                <i class="icofont-edit"></i>
-                                            </a>
-                                            @if(auth()->id() !== $user->id)
-                                            <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this user?');">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete">
-                                                    <i class="icofont-ui-delete"></i>
-                                                </button>
-                                            </form>
-                                            @endif
-                                        </div>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    
-                    <div class="mt-4">
-                        {{ $users->links() }}
-                    </div>
+                                    @endforeach
+                                </td>
+                                <td>
+                                    <div class="d-flex">
+                                        <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-info btn-sm mr-2" title="Edit">
+                                            <i class="icofont-edit"></i>
+                                        </a>
+                                        @if(auth()->id() !== $user->id)
+                                        <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this user?');">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm" title="Delete">
+                                                <i class="icofont-ui-delete"></i>
+                                            </button>
+                                        </form>
+                                        @endif
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="px-30 pb-30 mt-4">
+                    {{ $users->links() }}
                 </div>
             </div>
         </div>
